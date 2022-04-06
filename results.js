e801d82368a7b46c7b43dc8dea24a8bed02d7588
Salia-Monica <saliamonica78@gmail.com>
2022-04-06 14:47:40 +0000

    my first commit

---
+++ results.js
@@ -0,0 +1,12 @@
+const results=[
+    {name:"Windy Dery", class:"Form One", marks:8},
+    {name:"Amoa Joe", class:"Form One", marks:6},
+    {name:"Dery Kofi", class:"Form One", marks:5},
+    {name:"Kosi Kwame", class:"Form One", marks:9},
+    {name:"Windy Key", class:"Form One", marks:4},
+    {name:" Kojo Dery", class:"Form One", marks:7},
+
+]
+
+module.exports=results
+
